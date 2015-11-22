34d2c1d321f652e1e0f8f9d9f574ac8a3b47d0fb
Tony Wu <tony@fiftythree.com>
2015-11-22 17:38:56 -0500

    Identifies adv packet with Android

--- fw/softdevice_sdk/nrf51_sdk_v6_1_0_b2ec2e6/nrf51822/Board/pca10001/s110/ble_app_beacon/main.c
+++ fw/softdevice_sdk/nrf51_sdk_v6_1_0_b2ec2e6/nrf51822/Board/pca10001/s110/ble_app_beacon/main.c
@@ -43,13 +43,18 @@
 #define APP_ADV_DATA_LENGTH              0x15                              /**< Length of manufacturer specific data in the advertisement. */
 #define APP_DEVICE_TYPE                  0x02                              /**< 0x02 refers to Beacon. */
 #define APP_MEASURED_RSSI                0xC3                              /**< The Beacon's measured RSSI at 1 meter distance in dBm. */
-#define APP_COMPANY_IDENTIFIER           0x004C                            /**< Company identifier for Apple Inc. as per www.bluetooth.org. */
-#define APP_MAJOR_VALUE                  0x01, 0x02                        /**< Major value used to identify Beacons. */ 
-#define APP_MINOR_VALUE                  0x03, 0x04                        /**< Minor value used to identify Beacons. */ 
-#define APP_BEACON_UUID                  0x01, 0x12, 0x23, 0x34, \
-                                         0x45, 0x56, 0x67, 0x78, \
-                                         0x89, 0x9a, 0xab, 0xbc, \
-                                         0xcd, 0xde, 0xef, 0xf0            /**< Proprietary UUID for Beacon. */
+#define APP_COMPANY_IDENTIFIER           0x4C4C                            /**< Company identifier for Apple Inc. as per www.bluetooth.org. */
+
+
+
+
+#define APP_MAJOR_VALUE                  0x12, 0x34                        /**< Major value used to identify Beacons. */
+#define APP_MINOR_VALUE                  0xab, 0xcd                       /**< Minor value used to identify Beacons. */
+
+#define APP_BEACON_UUID                  0xFF, 0x00, 0x00, 0x00, \
+                                         0x00, 0x00, 0x00, 0x00, \
+										 0x00, 0x00, 0x00, 0x00, \
+                                         0x00, 0x00, 0x00, 0xFF
 
 #define DEAD_BEEF                        0xDEADBEEF                        /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
